ce757604ce8a4eb67bc0f23c49797aa4f3c1bb76
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 19:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const squares= document.querySelectorAll(".square");
+
+squares.forEach(square => {
+	square.addEventListener("mouseover", (event) => {
+		squares.forEach(square1 => {
+			if(event.target !== square1) {
+				console.log(event.target, square1);
+				square1.style.backgroundColor = "#6F4E37";	
+			}
+		})
+	})
+})
+
+squares.forEach(square => {
+	square.addEventListener("mouseout", (event) => {
+		squares.forEach(square1 => {
+			square1.style.backgroundColor = "#E6E6FA";
+		})
+	})
+})
